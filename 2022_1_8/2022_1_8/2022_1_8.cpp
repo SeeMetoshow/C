fe1a84a1aa7014476298a447f66a318cdf840939
SeeMetoshow <1991896378@qq.com>
2022-01-09 09:57:34 +0800

    2022年1月8日的c语言项目练习

---
+++ 2022_1_8/2022_1_8/2022_1_8.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+// 什么是语句 ？ 以一个分号 ; 结尾的就是一个语句
+// 分支语句（选择结构） -- if 和 switch
+//int main()
+//{
+//	int a = 10;
+//	// if 语句 在只跟一条 语句的情况下，可以不用 大括号
+//	// else 也是，，
+//	// if 和 else语句 只能控制一条语句，想让他们控制多条语句的话就用大括号
+//
+//	// 单分支 -- 可以 只有 一个 if 语句 ，后面不跟else
+//	/*if (a >= 18)
+//		printf("成年\n");*/
+//
+//	// 双分支
+//	/*if (a >= 18)
+//		printf("成年\n");
+//	else
+//		printf("未成年\n");*/
+//
+//	// 多分支
+//	//if (a < 18)
+//	//	printf("少年\n");
+//	//else if (a >= 18 && a < 26) // 也可以只写 a < 26
+//	//	printf("青年\n");
+//	//else if (a >= 26 && a < 40)
+//	//	printf("中年\n");
+//	//else if (a >= 40 && a < 60)
+//	//	printf("壮年\n");
+//	//else
+//	//	printf("老年\n");
+//
+//	// else 和 离他最近的 if 匹配
+//	return 0;
+//}
+
